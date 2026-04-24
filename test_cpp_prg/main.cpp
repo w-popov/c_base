1de02aad5693488e4b54c51c2a1243804de2d564
w-popov <mechanic.ep.su@gmail.com>
2026-04-24 09:55:23 +0300

    Настройка

--- test_cpp_prg/main.cpp
+++ test_cpp_prg/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 int main() {
+    std::setlocale(LC_ALL, "Russian");
     Kramer::matrix3x3 coefficients = {
         3., -2., 4.,
         3., 4., -2.,
@@ -16,12 +17,12 @@ int main() {
     Kramer::Kramer linear{coefficients, constants};
     auto solution = linear.solve();
 
-    std::cout << "\nSolution: ";
+    std::cout << "\n(Решение) Solution: ";
     for (const auto& value : solution) {
         std::cout << "\033[32m" << value << " " << "\033[0m";
     }
 
-    std::cout << std::endl << "Determinant: " << "\033[34m" << 
+    std::cout << std::endl << "(Определитель) Determinant: " << "\033[34m" << 
     linear.computeDeterminant(coefficients) << "\033[0m" << "\n\n";
 
     return 0;
