4b9ddd1a77928bf716dbd4a0e3f048b0457f4845
w-popov <mechanic.ep.su@gmail.com>
2026-05-09 09:37:23 +0300

    Домашние задание №3.1 введение в Си

---
+++ home_work_3_1/A1.c
@@ -0,0 +1,18 @@
+/**
+ * Задача A1:
+ * Вывести на экран текст "лесенкой":
+ *	 Let's
+ *     go
+ *      to walk
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("	 Let's\n");
+    printf("           go\n");
+    printf("             to walk");
+
+    return 0;
+}
