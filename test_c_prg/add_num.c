bd5dcdec84800771d684640da55a7cf7344bd3e2
w-popov <mechanic.ep.su@gmail.com>
2026-05-19 10:58:09 +0300

    remove 2 folder from index

--- test_c_prg/add_num.c
+++
@@ -1,6 +0,0 @@
-#include "some_head.h"
-
-int add_nums(int a, int b)
-{
-    return a + b;
-}
--- test_c_prg/some_head.h
+++
@@ -1,6 +0,0 @@
-#ifndef SOME_HEAD_H
-#define SOME_HEAD_H
-
-int add_nums(int a, int b);
-
-#endif // SOME_HEAD_H
