5c36370a88c2e9791723d38ba7d45416c6b1a360
w-popov <mechanic.ep.su@gmail.com>
2026-05-21 18:31:36 +0300

    переименование с home_work_3_1

---
+++ HW1/A0.c
@@ -0,0 +1,12 @@
+/**
+ * Задача A0:
+ * На стандартном потоке ввода напечатайте Hello world!. 
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ HW1/A2.c
@@ -0,0 +1,23 @@
+/**
+ * Задача A2:
+ * Вывести на экран рисунок из букв
+ *    *
+ *   ***
+ *  *****
+ * *******
+ *  HHOHH
+ *  ZZZZZ
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("   HHOHH\n");
+    printf("   ZZZZZ\n");
+    return 0;
+}
