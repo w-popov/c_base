80b5aeda86ec026a7e8cddd693d4f92688dd2508
w-popov <mechanic.ep.su@gmail.com>
2026-06-21 16:43:47 +0300

    Сделано F1-F17

--- HW9/src/F16.c
+++ HW9/src/F16.c
@@ -1,18 +1,72 @@
 /**
  * ДЗ-9. Си базовый уровень. гр.Д01-134 Попов. В.Г
- * F16: 
+ * F16: Известно, что шахматная доска имеет 
+ *      размерность 8х8 и состоит из клеток 2х цветов...
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-
+/**
+ * A4 => [col, row]
+ * Если: row - чет и  | col - нечет, то white
+ *                    | col - чет, то black
+ * 
+ * Если row - нечет и | col - нечет, то black
+ *                    | col - чет, то white
+ */
+const char* black_or_white (char* coords)
+{
+    if (!coords || strlen(coords) != 2 || !(isalpha(coords[0]) && isdigit(coords[1])))
+    {
+        return "Error coordinates\n";
+    }
+    /* Буква маленькая */
+    coords[0] = tolower(coords[0]);
+    if ( !(coords[0] >= 'a' && coords[0] <= 'h') )
+    {
+        return "Error colums coordinates\n";
+    }
+    
+    enum { BOARD = 9 };
+    char cols[BOARD] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '\0'};
+    char *pt_col = strchr(cols, coords[0]);
+    if (pt_col == NULL)
+    {
+        return "Error searching index\n";
+    }
+    /* Координаты col, row: */
+    int col = pt_col - cols;
+    int row = coords[1] - '0';
+    if (row % 2)
+    {
+        if (col % 2)
+        {
+            return "WHITE";
+        }
+        return "BLACK";
+    }
+    else if (!(row % 2))
+    {
+        if (col % 2)
+        {
+            return "BLACK";
+        }
+    }
+    return "WHITE";
+}
 
 #ifndef TEST_DEF_HW9
 int main (void)
 {
-    enum { SIZE_ARR = 10 };
-    int array[SIZE_ARR] = {0};
-    printf("%d\n", array[0]);
+    enum { SIZE_ARR = 4 };
+    char coordinates[SIZE_ARR] = {'\0'};
+    if (scanf("%s", coordinates) != 1)
+    {
+        return EXIT_FAILURE;
+    }
+    printf("%s\n", black_or_white(coordinates));
 
     return EXIT_SUCCESS;
 }
--- HW9/src/F17.c
+++ HW9/src/F17.c
@@ -1,19 +1,54 @@
 /**
  * ДЗ-9. Си базовый уровень. гр.Д01-134 Попов. В.Г
- * F17: 
+ * F17: Составить функцию которая находит след матрицы 
+ *      в двумерном массиве. Показать пример ее 
+ *      работы на матрице из 5 на 5 элементов.
  */
 #include <stdio.h>
 #include <stdlib.h>
 
+enum { SIZE_MATIX = 5 };
 
+int trace_of_matrix (int matrix[][SIZE_MATIX], const int size)
+{
+    int trace_sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        trace_sum += matrix[i][i];
+    }
+    return trace_sum;
+}
+
+/**
+ * Заполнить двумерный массив
+ * @return Указатель на двумерный массив
+ */
+int (*fill_matrix_F17 (int matrix[][SIZE_MATIX], const int size))[SIZE_MATIX]
+{
+    for (int row = 0; row < size; ++row)
+    {
+        for (int col = 0; col < size; ++col)
+        {
+            if (scanf("%d", &matrix[row][col]) != 1)
+            {
+                return NULL;
+            }
+        }
+    }
+    return matrix;
+}
 
 #ifndef TEST_DEF_HW9
 int main (void)
 {
-    enum { SIZE_ARR = 10 };
-    int array[SIZE_ARR] = {0};
-    printf("%d\n", array[0]);
-
+    int array[SIZE_MATIX][SIZE_MATIX] = {0};
+    int (*matrix)[SIZE_MATIX] = fill_matrix_F17(array, SIZE_MATIX);
+    if (matrix == NULL)
+    {
+        printf("Fail fill matrix\n");
+        return EXIT_FAILURE;
+    }
+    printf("%d\n", trace_of_matrix(matrix, SIZE_MATIX));
     return EXIT_SUCCESS;
 }
 #endif
